c82ad1a83af2f29b51cc79db7b1388098f30df15
DELUCKS <sl@delucks.com>
2018-04-28 09:40:11 +0200

    Breadcrumbs & Sharing Buttons unseres SEO Plugins

---
+++ seoplugin.php
@@ -0,0 +1,8 @@
+//* Breadcrumbs einfügen
+
+<?php echo function_exists('dpc_breadcrumbs') ? dpc_breadcrumbs() : ''; ?>
+
+
+//* Sharing Buttons einfügen 
+
+<?php echo function_exists('dpc_sharing') ? dpc_sharing() : ''; ?>
