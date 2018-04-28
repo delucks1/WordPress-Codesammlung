1ab6abafb90dc7f15720e836c28aa17ad01323e2
DELUCKS <sl@delucks.com>
2018-04-28 09:30:50 +0200

    Mehrsprachige Inhalte steuern

---
+++ WPML.php
@@ -0,0 +1,11 @@
+//* Eine if else Funktionen zum Steuern von mehrsprachigen Inhalten im Theme
+
+<?php if(ICL_LANGUAGE_CODE=='de'): ?>
+
+Deutscher Text
+
+<?php elseif(ICL_LANGUAGE_CODE=='en'): ?>
+
+Englischer Text
+		
+<?php endif; ?>
