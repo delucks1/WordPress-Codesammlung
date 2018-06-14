0aababa3b40dbab5115b80406ebc7fd45ba9da6b
DELUCKS <sl@delucks.com>
2018-06-14 14:27:23 +0200

    Update index.php
    Seitenspezifisch Inhalte ausspielen

--- index.php
+++ index.php
@@ -6,11 +6,13 @@
   Inhalte für andere Seiten
 <?php } ?>
 
+
 //* Inhalt für spezielle Seite hinzufügen
 
 <?php if ( is_page( '2461' )  ): ?>
   Inhalt für spezielle Seite
 <?php endif ?>
+
 
 //* Inhalte für alle Seiten als auf der Startseite hinzufügen (durch Codeverneinung)
 
@@ -18,8 +20,24 @@
   Inhalte für andere Seiten
 <?php } ?>
 
+
 //* Inhalte für die Blogübersicht hinzufügen
 
 <?php if(is_home() && !is_front_page() && !is_paged() ) : ?>
   Inhalte für Blogübersicht
+<?php endif; ?>
+
+
+//* In Blogübersicht für Sticky Posts extra das Beitragsbild darstellen
+
+<?php if ( is_sticky()): ?>
+<a href="<?php echo get_permalink( $post->ID ); ?>">
+	<div style="
+				background-image: url(<?php echo get_the_post_thumbnail_url($post_id, 'thumbnail'); ?>);
+				width: 100%;
+				height: 100%;
+				background-size: cover;
+				background-position: center;			
+				"></div>
+</a>
 <?php endif; ?>
