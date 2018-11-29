3692f2dc258bb0a27d6ade2014f238149f7064b1
DELUCKS <sl@delucks.com>
2018-11-29 08:47:19 +0100

    Update seoplugin.php

--- seoplugin.php
+++ seoplugin.php
@@ -1,8 +1,35 @@
-//* Breadcrumbs einfügen
+/* --- Metadaten ---*/
 
+//* PHP-Code für Beschreibungen
+<?php echo function_exists('dpc_metadescription') ? dpc_metadescription() : ''; ?>
+
+//* Shortcode für Beschreibungen
+[dpc_metadescription]
+
+
+/* --- Statistiken --- */
+
+//* PHP-Code Opt-Out für Analytics
+<?php echo function_exists('dpc_analytics_opt_out') ? dpc_analytics_opt_out() : ''; ?>
+
+//* Shortcode Opt-Out für Analytics
+[dpc_analytics_opt_out link_text='']
+
+/* --- Breadcrumbs --- */
+
+//* PHP-Code
 <?php echo function_exists('dpc_breadcrumbs') ? dpc_breadcrumbs() : ''; ?>
 
+//* Shortcode
+[dpc_breadcrumbs]
 
-//* Sharing Buttons einfügen 
+/* --- Bewertungen --- */
 
+//* PHP-Code
 <?php echo function_exists('dpc_sharing') ? dpc_sharing() : ''; ?>
+
+//* Shortcode
+[dpc_rating]
+
+//* Shortcode mit geändertem HTML-Anker
+[dpc_rating alt_link=#HTML-Anker]
